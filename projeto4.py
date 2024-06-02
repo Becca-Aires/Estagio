6fca25b6bd1d3d8f606fcaa9926f29901c8b4155
Rebbeca Aires <rebbecaribeiro1@gmail.com>
2024-06-02 15:58:43 -0300

    Exercise 4

---
+++ projeto4.py
@@ -0,0 +1,6 @@
+'''Crie um programa que solicita ao usuário um valor em reais e converta esse número para dólares, considerando a cotação de 
+1 dólar = 5 reais.'''
+
+valor_reais = float(input('Digite o valor em reais: '))
+dolar = valor_reais / 5
+print ('Valor em dólar', dolar)
